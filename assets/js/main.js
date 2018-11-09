54dfa835f789cfd08c8757bc91624a7431af3a3e
Umpoy <iumpoy@yahoo.com>
2018-11-08 21:50:07 -0800

    boxes created dynamically

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,21 @@ $(document).ready(initialize);
 
 function initialize(){
 	const board = new Board();
-	board.createBoxes();
+	board.createBoxes(5,5);
 }
 
-function Board() {
-	this.createBoxes = function() {
-		console.log('hello');
+function Board(num1,num2) {
+	
+	this.createBoxes = function(num1, num2) {
+		const board = $('<div>').addClass('board');
+		const box = $('<div>').addClass('box');
+		$('body').append(board);
+		console.log($(board))
+		for(let i = 0; i < num1; i++){
+			for(let j = 0; j < num2; j++){
+				$('.board').append(box);
+			}
+		}
+
 	}
 }
