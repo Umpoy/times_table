31a1ac7436d8f14d03a3bb3bb20a689fc2eadde3
Umpoy <iumpoy@yahoo.com>
2018-11-10 19:03:10 -0800

    added css grid and numbers to box

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,15 +9,19 @@ function initialize(){
 function Board(num1,num2) {
 	
 	this.createBoxes = function(num1, num2) {
-		const board = $('<div>').addClass('board');
+		// const div = $('<div>');
+		const timesBoard = $('<div>').addClass('board');
 		const box = $('<div>').addClass('box');
-		$('body').append(board);
-		console.log($(board))
+		$('body').append(timesBoard);
+		let count = 0;
 		for(let i = 0; i < num1; i++){
 			for(let j = 0; j < num2; j++){
+				count++
+				box.html(count)
 				$('.board').append(box);
+				
 			}
 		}
-
+		console.log(count);
 	}
 }
